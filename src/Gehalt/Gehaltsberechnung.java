ee127887f399774dfdab35a58fe38a8805b89f76
nils-imhoff <github@d4d1.de>
2019-06-01 21:31:53 +0200

    improve Gehaltsberechnung

--- src/Gehalt/Gehaltsberechnung.java
+++ src/Gehalt/Gehaltsberechnung.java
@@ -1,42 +1,17 @@
 package Gehalt;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Gehaltsberechnung {
-
-    private List<Mitarbeiter> aMitarbeiter;
-
-    public Gehaltsberechnung(List<Mitarbeiter> aMitarbeiter) {
-        this.aMitarbeiter = aMitarbeiter;
-    }
-
-    public int berechneAlleGehaelter() {
-        int result = 0;
-        for (Mitarbeiter mitarbeiter : aMitarbeiter) {
-            result += mitarbeiter.berechneGehalt();
-        }
-        return result;
-    }
-
-    public int berechneAlleGehaelter8() {
-        return aMitarbeiter.stream()
-                .map(a -> a.berechneGehalt())
-                .reduce((a1, a2) -> a1 + a2).orElseGet(() -> 0);
-    }
-
-
-    public static List<Mitarbeiter> aMitarbeiter() {
-        List<Mitarbeiter> result = new ArrayList<>();
-        result.add(new Arbeiter(10, 150));
-        result.add(new Angestellter(3000, 1000));
-        return result;
-    }
-
+    
     public static void main(String[] args) {
-        List<Mitarbeiter> bMitarbeiter = aMitarbeiter();
+        Arbeiter aMitarbeiter = new Arbeiter(10, 40);
+        Angestellter bMitarbeiter = new Angestellter(5000, 300 );
+        Geschaeftsfuehrer cMitarbeiter = new Geschaeftsfuehrer(10000, 1000, 500, 700);
 
-        Gehaltsberechnung gehaltsberechnung = new Gehaltsberechnung(bMitarbeiter);
-        System.out.println(gehaltsberechnung.berechneAlleGehaelter());
+        double Lohn = aMitarbeiter.berechneGehalt();
+        double Gehalt = bMitarbeiter.berechneGehalt();
+        double bGehalt = cMitarbeiter.berechneGehalt();
+        System.out.println("Arbeiter: " + Lohn + " Angestellter: " + Gehalt +" Geschaeftsfuehrer: " + bGehalt);
     }
 }
